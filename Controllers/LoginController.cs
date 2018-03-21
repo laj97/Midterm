6dd9c7ab88adb934d787b5e111c418f001beb618
Luis Jimenez <Guero@Luiss-MacBook-Pro-2.local>
2018-03-21 12:13:00 -0500

    LoginViewModel

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Midterm1.Models;
+
+namespace Midterm1.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LoginSuccess()
+        {
+            ViewData["Message"] = "You've logged in successfully";
+
+            return View();
+        }
+
+        public IActionResult LoginFail()
+        {
+            ViewData["Message"] = "Login Failed";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Midterm1.Models{
+
+    public class LoginView
+    {
+
+        public string Username{ get; set; }
+        public string password{ get; set; }
+    }
+}
